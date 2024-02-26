237274423f25eb719809083990707102a2c4701e
amoriah <yonoyko@bk.ru>
2024-02-26 12:23:12 +0300

    fiels minimum validations

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,11 +1,11 @@
-import { Col, Row, Typography } from "antd";
-import { Content } from "antd/es/layout/layout";
-import { observer } from "mobx-react-lite";
-import components from "../components";
-import { useStore } from "../store/RootStore";
-import { UserModelType } from "../store/usersStore";
-import * as style from "../style/HotelsStyle";
-import { v4 as uuidv4 } from "uuid";
+import { Col, Row, Typography } from 'antd';
+import { Content } from 'antd/es/layout/layout';
+import { observer } from 'mobx-react-lite';
+import components from '../components';
+import { useStore } from '../store/RootStore';
+import { UserModelType } from '../store/usersStore';
+import * as style from '../style/HotelsStyle';
+import { v4 as uuidv4 } from 'uuid';
 
 const { Text, Title } = Typography;
 
@@ -21,36 +21,58 @@ const Component = observer(() => {
 
   const accountFieldsItems: fieldType[] = [
     {
-      title: "Логин",
-      key: "login",
+      title: 'Логин',
+      key: 'login',
       value: session.session?.login,
     },
     {
-      title: "Имя",
-      key: "name",
+      title: 'Имя',
+      key: 'name',
       value: session.session?.name,
     },
     {
-      title: "Возраст",
-      key: "age",
+      title: 'Возраст',
+      key: 'age',
       value: session.session?.age,
     },
     {
-      title: "Телефон",
-      key: "phone",
+      title: 'Телефон',
+      key: 'phone',
       value: session.session?.phone,
     },
   ];
 
-  const fieldsItems = accountFieldsItems.map((item) => {
+  function isNumeric(str: string) {
+    return /^\d+$/.test(str);
+  }
+
+  const changeHandle = (value: string, key: keyof UserModelType) => {
+    if (key === 'phone') {
+      if (!isNumeric(value) || value.length !== 11) {
+        alert('Введите правильный номер');
+        return;
+      }
+    }
+    if (key === 'age') {
+      const num = Number(value);
+      if (!isNumeric(value) || num > 130) {
+        alert('Введите реальный возраст');
+        return;
+      }
+      setAccountValue(value, session.session!.userId, key);
+    }
+  };
+
+  const fieldsItems = accountFieldsItems.map(item => {
     return (
       <Row style={style.accountFieldStyle} key={uuidv4()}>
         <Title level={4}>{item.title}</Title>
         <Text
           editable={{
             onChange: (value: string) => {
-              setAccountValue(value, session.session!.userId, item.key);
+              changeHandle(value, item.key);
             },
+            onStart: () => console.log(),
           }}
           style={style.accountTextStyle}
         >
@@ -61,7 +83,7 @@ const Component = observer(() => {
   });
 
   return (
-    <Content style={{ margin: "0 100px" }}>
+    <Content style={{ margin: '0 100px' }}>
       <Col>{fieldsItems}</Col>
       <components.HistoryTable />
     </Content>
